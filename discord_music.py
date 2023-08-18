72f087cf8bc7038e8786f0fb0cc98998c0190b39
Edward Lo <ed700a@gmail.com>
2023-08-19 00:52:15 +0800

    Add files via upload

--- discord_music.py
+++ discord_music.py
@@ -3,6 +3,8 @@ import io
 
 import discord
 from discord.ext import commands, tasks
+from discord.utils import get
+from discord.voice_client import VoiceClient
 import youtube_dl
 import random
 
@@ -16,6 +18,7 @@ async def on_ready():
     # Start the timeout check loop
     check_voice_channel.start()"""
 
+
 ### Basics ###
 async def join(ctx):
     if ctx.author.voice:
@@ -25,18 +28,21 @@ async def join(ctx):
     else:
         await ctx.send("You must be in a voice channel!")
 
+
 async def leave(ctx):
     if ctx.voice_client:
         await ctx.voice_client.disconnect()
         await ctx.send("Leaving VC")
     else:
         await ctx.send("I am not connected to a voice channel.")
-        
+
+
 async def join_error(ctx, error):
     if isinstance(error):
         await ctx.send(str(error))
-        
-#@tasks.loop(seconds=5)
+
+
+# @tasks.loop(seconds=5)
 """async def check_voice_channel():
     print('loop')
     for voice_client in bot.voice_clients:
@@ -47,3 +53,52 @@ async def join_error(ctx, error):
 
 async def before_check_voice_channel():
     await bot.wait_until_ready()"""
+
+sources = ["The Last Chance (Rio & Aoi Version).mp3"]
+
+
+async def custom_probe(source, executable):
+    codec = "opus"
+    bitrate = 128
+
+    return codec, bitrate
+
+
+async def play(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        voice_client: VoiceClient = get(bot.voice_clients, guild=ctx.guild)
+
+        if voice_client and voice_client.is_connected():
+            if voice_client.channel == channel:
+                await ctx.send("I am already in your voice channel.")
+            else:
+                await voice_client.move_to(channel)
+                await ctx.send(f"Moved to {channel.name}")
+        else:
+            voice_client = await channel.connect()
+            await ctx.send(f"Entering {channel.name}")
+
+        to_play = random.choice(sources)
+
+        # OpusAudio.from_probe
+        """source = await discord.FFmpegOpusAudio.from_probe(
+            to_play,
+            executable=r"D:/coding_workspace/Discord/ffmpeg-6.0-essentials_build/bin/ffmpeg.exe",
+            method="fallback",
+        )
+        voice_client.play(source)"""
+
+        # PC Audio
+        voice_client.play(
+            (
+                discord.FFmpegPCMAudio(
+                    executable=r"D:/coding_workspace/Discord/ffmpeg-6.0-essentials_build/bin/ffmpeg.exe",
+                    source=to_play,
+                )
+            ),
+            after=lambda e: print("Done", e),
+        )
+        await ctx.send(f"Playing: {to_play}")
+    else:
+        await ctx.send("You must be in a voice channel!")
